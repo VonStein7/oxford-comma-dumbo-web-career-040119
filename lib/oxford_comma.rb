6adaf3079ea494a3a0d83ad214dbad1f72518673
brenden williams <brendenw7@gmail.com>
2019-03-26 01:42:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.join()
+  end
+  
+  if array.length == 2 
+    return array.join(' and ')
+  end 
+  
+  last = ", and #{array.pop}"
+  
+  array.join(", ") + last
 end
